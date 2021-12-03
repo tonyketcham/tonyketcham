fd22fdf7c36760c74f4055168c387c7fdf019223
Tony Ketcham <ketcham@pnw.edu>
2021-12-03 00:50:10 -0600

    fix build

--- src/lib/components/TreeScene.svelte
+++ src/lib/components/TreeScene.svelte
@@ -3,24 +3,29 @@
 	import * as SC from 'svelte-cubed';
 	import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 	import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+	import { assets } from '$app/paths';
+	import { browser } from '$app/env';
 
-	const url = '/Lowpoly_tree_sample';
 	let mesh: THREE.Mesh;
+	const mtlSrc = `${assets}/Lowpoly_tree_sample.mtl`;
 
 	const mtlLoader = new MTLLoader();
 	const objLoader = new OBJLoader();
 
-	// Load the object and material resources
-	mtlLoader.load(url + '.mtl', (mtl) => {
-		mtl.preload();
-		objLoader.setMaterials(mtl);
+	if (browser) {
+		// Load the object and material resources
+		mtlLoader.load(mtlSrc, (mtl) => {
+			const objSrc = `${assets}/Lowpoly_tree_sample.obj`;
+			mtl.preload();
+			objLoader.setMaterials(mtl);
 
-		objLoader.load(url + '.obj', (event) => {
-			mesh = event.children[0] as THREE.Mesh;
-			console.log(mesh);
-			return mesh;
+			objLoader.load(objSrc, (event) => {
+				mesh = event.children[0] as THREE.Mesh;
+				console.log(mesh);
+				return mesh;
+			});
 		});
-	});
+	}
 
 	async function loadMesh(mesh: THREE.Mesh) {
 		return new Promise((resolve: (value: THREE.Mesh) => any) => {
@@ -40,7 +45,7 @@
 	});
 </script>
 
-<SC.Canvas antialias background={new THREE.Color('transparent')} shadows>
+<SC.Canvas antialias alpha shadows>
 	{#await meshPromise then mesh}
 		<SC.Mesh
 			geometry={mesh.geometry}
@@ -51,7 +56,7 @@
 		/>
 	{/await}
 	<SC.PerspectiveCamera position={[5, 5, 30]} />
-	<SC.OrbitControls enableZoom={true} maxPolarAngle={Math.PI * 0.51} />
+	<SC.OrbitControls maxPolarAngle={Math.PI * 0.51} />
 	<SC.AmbientLight intensity={0.6} />
 	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} shadow={{ mapSize: [2048, 2048] }} />
 </SC.Canvas>
