c508e827062d7e412bbaf82483f2d486af18cfc5
Tony <tonyketcham@gmail.com>
2021-12-06 14:24:53 -0600

    feat: fix 404'd links

---
+++ src/routes/art.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>Tony's Art 🧙‍♂️</title>
+	<meta name="description" content="a portfolio of sorts" />
+</svelte:head>
+
+<article class="p-4">
+	<section class="space-y-5 text-lg">
+		<h1 class="text-6xl mb-2">Art + Design</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exercitationem ab nemo explicabo
+			delectus, eligendi quisquam eveniet adipisci? Commodi sapiente iusto facere doloremque
+			repellat pariatur possimus ipsa veniam, corporis velit.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exercitationem ab nemo explicabo
+			delectus, eligendi quisquam eveniet adipisci? Commodi sapiente iusto facere doloremque
+			repellat pariatur possimus ipsa veniam, corporis velit.
+		</p>
+	</section>
+</article>
